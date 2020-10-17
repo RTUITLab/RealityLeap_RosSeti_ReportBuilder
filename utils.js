22ec70c661d5e077d82202126c3d5f59ffe20b15
Vladimir <vovcrane@mai.ru>
2020-10-17 17:50:15 +0300

    kill bugs

--- utils.js
+++ utils.js
@@ -42,9 +42,13 @@ const employeeToFullname = (employee) =>
 
 const timestampToString = function (timestamp) {
     let date = new Date(timestamp * 1000);
-    let hours = date.getHours();
-    let minutes = "0" + date.getMinutes();
-    let seconds = "0" + date.getSeconds();
-    return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    // let hours = date.getHours();
+    // let minutes = "0" + date.getMinutes();
+    // let seconds = "0" + date.getSeconds();
+    // return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    let day = "0" + date.getDay();
+    let month = "0" + date.getMonth();
+    let year = date.getFullYear();
+    return day.substr(-2) + '.' + month.substr(-2) + '.' + year;
 }
 
